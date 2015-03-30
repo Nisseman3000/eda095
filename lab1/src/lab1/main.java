4ca3bd4f340b9248b3acc4cdd5144d5fd08d8931
g3ws <dat11jja@student.lu.se>
2015-03-30 17:59:42 +0200

    lab1 funkar

--- lab1/src/lab1/main.java
+++ lab1/src/lab1/main.java
@@ -1,9 +1,13 @@
 package lab1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,11 +33,23 @@ public class main {
 				}
 				
 			}
+			bReader.close();
 			for (String s : urls) {
-				//System.out.println(s);
 				URL tempURL = new URL(myDoc,s);
-				System.out.println(tempURL);
+				//System.out.println(tempURL);
+				Path p = Paths.get(tempURL.getPath());
+				String file = p.getFileName().toString();
+				System.out.println(file);
+				InputStream in = tempURL.openStream();
+				FileOutputStream fos = new FileOutputStream(new File(file));
+				int length = -1;
+				byte[] buffer = new byte[1024];// buffer for portion of data from
+				                                // connection
+				while ((length = in.read(buffer)) > -1) {
+				    fos.write(buffer, 0, length);
+				}
 			}
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
