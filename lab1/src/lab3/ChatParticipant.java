2c8e8e77d09103d565403aadfa674ba5a8f90562
arturmatulaniec <dat11ama@student.lu.se>
2015-04-12 19:26:03 +0200

    La till en klass ChatParticipant, läs kommentaren om ni hinner titta innan jag e tillbaka.

---
+++ lab1/src/lab3/ChatParticipant.java
@@ -0,0 +1,18 @@
+package lab3;
+
+public class ChatParticipant {
+	
+	private String name;
+	/* Om ni läser detta, när man kopplade upp sig mot en klient via telnet och trådmässigt så fick man ett
+	 * annat portnummer, vi kanske kan använda det för att kunna hitta alla? 
+	 * 
+	 * alltså socket.getPort(), så den ger rätt port till varje telnet client. 
+	 * 
+	 */
+	
+	public ChatParticipant(){
+		
+		
+	}
+	
+}
