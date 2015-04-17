2bd8ed92e1275b4bba3d04e6900ee881c2d23358
g3ws <dat11jja@student.lu.se>
2015-04-17 23:34:56 +0200

    små fix

--- lab1/src/lab4/WebCrawlerMain.java
+++ lab1/src/lab4/WebCrawlerMain.java
@@ -14,24 +14,22 @@ import org.jsoup.select.Elements;
 
 public class WebCrawlerMain {
 	private static final int MAX_PAGES = 100;
-	private HashSet<URL> visitedPages;
+	private HashSet<URL> collectedPages;
 	private LinkedList<URL> notVisitedPages;
 	private HashSet<String> emails;
 	private Elements frames;
 
-	void parse() throws IOException {
+	void crawl() throws IOException {
 		notVisitedPages = new LinkedList<URL>();
-		visitedPages = new HashSet<URL>();
+		collectedPages = new HashSet<URL>();
 		emails = new HashSet<String>();
 		URL url = new URL("http://cs.lth.se/eda095/");
 
-		// System.out.println(doc.toString());
-		searchPage(url);
+		crawlPage(url);
 
 		System.out.println("Links:");
-		
-		for (URL s : visitedPages)
-		System.out.println(s.toString());
+		for (URL s : collectedPages)
+			System.out.println(s.toString());
 
 		System.out.println("Emails:");
 		for (String s : emails)
@@ -39,24 +37,26 @@ public class WebCrawlerMain {
 
 	}
 
-	public void searchPage(URL url) {
+	public void crawlPage(URL url) {
 		notVisitedPages.add(url);
-		while (notVisitedPages.size() < MAX_PAGES && notVisitedPages.size() > 0) {
+		while (collectedPages.size() < MAX_PAGES && notVisitedPages.size() > 0) {
 			String urlString = url.toString();
 			Document doc = null;
 			boolean pageFound = false;
-			while(!pageFound){
-			try {
-				doc = Jsoup.connect(urlString).get();
-				pageFound = true;
-			} catch (IOException e) {
-				System.err.println("Error: \"" + urlString + "\" not found");
-				pageFound =false;
-			}
+			while (!pageFound) {
+				try {
+					doc = Jsoup.connect(urlString).get();
+					pageFound = true;
+				} catch (IOException e) {
+					System.err
+							.println("Error: \"" + urlString + "\" not found");
+					pageFound = false;
+				}
 			}
 			Elements base = doc.getElementsByTag("base");
 			String baseString = base.attr("href");
-			System.out.println("visitedpages.size() : " + visitedPages.size());
+			System.out
+					.println("visitedpages.size() : " + collectedPages.size());
 			System.out.println("notVisitedpages.size() : "
 					+ notVisitedPages.size());
 			System.out.println("EmailsSize: " + emails.size());
@@ -89,7 +89,7 @@ public class WebCrawlerMain {
 						}
 					}
 					if (!exist) {
-						visitedPages.add(newUrl);
+						collectedPages.add(newUrl);
 						notVisitedPages.addLast(newUrl);
 					}
 				} catch (MalformedURLException e0) {
@@ -100,14 +100,14 @@ public class WebCrawlerMain {
 				emails.add(email.attr("href"));
 			}
 
-			visitedPages.add(url);
+			collectedPages.add(url);
 			url = notVisitedPages.poll();
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		WebCrawlerMain wcm = new WebCrawlerMain();
-		wcm.parse();
+		wcm.crawl();
 	}
 
 }
